4be58ae40ed0543c2b04138d16aaab7267237589
Alex <alexer_2004@mail.ru>
2020-05-13 19:21:12 +0300

    Commit 5

--- src/com/VER_4/ScanTest.java
+++ src/com/VER_4/ScanTest.java
@@ -8,12 +8,12 @@ public class ScanTest {
 
     public static void main(String[] args) throws IOException {
 
-        //String[] arg = new String[]{"D:/Загрузки", "D:/", "Test"};  // для проверки не из консоли
+        String[] arg = new String[]{"C:/", "D:/", "Test"};  // для проверки не из консоли
         //String[] arg = new String[]{"D:/Загрузки", "D:/", "Test", "exe"};  // для проверки не из консоли
 
         try {
             ScanUtils.logExistingDrives();
-            ArgumentsSupplier argumentsSupplier = new CommandLineArgumentsSupplier(args);
+            ArgumentsSupplier argumentsSupplier = new CommandLineArgumentsSupplier(arg);
             Arguments arguments = argumentsSupplier.processArguments();
             Collection<ScanWriter> writers = List.of(new ConsoleDescriptionWriter(), new FileDescriptionWriter(arguments.getReportFile()));
             FileScanner scanner = new FileScanner(arguments, writers);
